c8ab998dd03c6fc5d7255bb897b7af07abf613a9
zukenlove <nailofrantz@gmail.com>
2025-11-11 20:44:00 -0500

    adding the models,py file, install myqslclient and configure the database

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.SmallIntegerField()
+    booking_date = models.DateTimeField(auto_now=True)
+    
+    
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    inventory = models.SmallIntegerField()
